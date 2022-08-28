562f73a0217d36fd38ef00de5feb631f3c1f293c
Renan Mougenot Breviglieri Silva <mougenot.renan@gmail.com>
2022-08-27 20:51:29 -0600

    Aug 27 2022

--- Javascript/database.js
+++ Javascript/database.js
@@ -1,7 +1,7 @@
 //Database for the WSU Black Soccer Players
 const coach = [["Renan Mougenot", "Head Coach"], ["Mansour Al Hajri", "Assistant Coach"], ["Justin Foster", "Goalkeeper Coach"]];
 const forward = [["Agustin Torena", "#"], ["Aziz Alarady", "#96"], ["Belal Glab", "#9"], ["Brandon Pearce", "#28"],["Emmanuel Perez", "#11"], 
-["Gabe Juarez", "#18"], ["Jumaah Alfaihani", "#94"], ["Luis Arce", "#30"], ["Will Naughton", "#45"]];
+["Gabe Juarez", "#18"], ["Jumaah Alfaihani", "#94"], ["Luis Arce", "#12"], ["Will Naughton", "#45"]];
 const mid = [["Asher Jensen", "#7"], ["Charly Nuñez", "#8"], ["Jake Williams", "#19"], ["Luis Jordan", "#17"],["Riley Wright", "#10"], 
 ["Teegan Smith", "#"]];
 const def = [["Ethan Barnette", "#3"], ["James Patten", "#14"], ["Jorge Benitez", "#26"], ["Kaden Hecker", "#22"],["Kolton Kitchin", "#24"]];
